c4f22e2f8bfbc0d0641476eebf0ad4e5efd10611
kot108 <118835535+kot108@users.noreply.github.com>
2022-12-11 18:09:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите число 1");
+string A = Console.ReadLine()!;
+
+Console.WriteLine("Введите число 2");
+string B = Console.ReadLine()!;
+
+Console.WriteLine("Введите число 3");
+string C = Console.ReadLine()!;
+
+int ParseA = int.Parse(A);
+
+int ParseB = int.Parse(B);
+
+int ParseC = int.Parse(C);
+
+int max = ParseA;
+
+
+if (ParseB > ParseA)
+{
+    max = ParseB;
+    
+}
+if (ParseC > ParseA)
+{
+    max = ParseC;
+
+}
+Console.WriteLine("Максимальное число =");
+
+Console.WriteLine(max);
+
+
